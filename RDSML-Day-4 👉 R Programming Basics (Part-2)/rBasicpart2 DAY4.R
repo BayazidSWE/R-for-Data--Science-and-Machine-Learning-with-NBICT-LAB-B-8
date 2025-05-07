9c1ac94a90f55a31479347a82614c6742667dc9e
BayazidSWE <bayazid.swe@gmail.com>
2025-05-07 22:50:36 +0600

    RDSML-Day-4

---
+++ RDSML-Day-4 👉 R Programming Basics (Part-2)/rBasicpart2 DAY4.R
@@ -0,0 +1,43 @@
+a=11
+b=7.19
+class(a)
+class(b)
+
+name="Bayazid Hossain"
+print(name)
+class(name)
+address="Dinajpur"
+print(a+9)
+
+#Logical Data Type
+a>b
+class(b>a)
+a<b
+x=19
+y=22-3
+print(x==y)
+print(x!=y)
+print(y>x)
+print(y>=x)
+class(y>=x)
+
+#Logical Operators in R
+# && (and) logical operator
+4==4 && 5==5
+4==4 && 4==5
+# x==y || a
+4==5 || 5==5
+#Simple Mathematical Operator
+z=6+9-8*(2/3)
+print(z)
+v=(4+3-3)*2/3
+print(v)
+b=4^3
+print(b)
+11/2
+11%%2
+11%/%2
+
+
+
+

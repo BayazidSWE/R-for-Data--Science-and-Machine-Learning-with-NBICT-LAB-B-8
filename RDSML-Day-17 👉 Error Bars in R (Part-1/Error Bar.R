e9c7115b7fde08d56c611de6cdbaacaf7902ba88
BayazidSWE <bayazid.swe@gmail.com>
2025-06-30 22:55:56 +0600

    Create Error Bar.R

---
+++ RDSML-Day-17 👉 Error Bars in R (Part-1/Error Bar.R
@@ -0,0 +1,18 @@
+#Importing a built-in dataset
+tooth_data=ToothGrowth
+
+#Creating a function for summarizing the data 
+#based on the grouping variables
+
+require(plyr)
+data_summary = function(data, varname, groupnames){
+  require(plyr)
+  summary_func = function(x, col){
+    c(mean = mean(x[[col]], na.rm = TRUE), sd = sd(x[[col]], na.rm = TRUE))
+  }
+  data_sum = ddply(data, groupnames, .fun = summary_func, varname)
+  data_sum = rename(data_sum, c("mean" = varname))
+  return(data_sum)}
+
+
+
